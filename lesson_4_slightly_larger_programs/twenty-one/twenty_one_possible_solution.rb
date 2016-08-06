f0a5448a143b97666323deb6c099f82f8a915195
Nick Geballe <ngeballe@gmail.com>
2016-08-06 11:50:55 -0700

    Address most Rubocop suggestions

--- lesson_4_slightly_larger_programs/twenty-one/twenty_one_possible_solution.rb
+++ lesson_4_slightly_larger_programs/twenty-one/twenty_one_possible_solution.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
-SUITS = ['H', 'D', 'S', 'C']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+SUITS = ['H', 'D', 'S', 'C'].freeze
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9',
+          '10', 'J', 'Q', 'K', 'A'].freeze
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -16,13 +17,13 @@ def total(cards)
 
   sum = 0
   values.each do |value|
-    if value == "A"
-      sum += 11
-    elsif value.to_i == 0 # J, Q, K
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value == "A"
+             11
+           elsif value.to_i == 0 # J, Q, K
+             sum += 10
+           else
+             sum += value.to_i
+           end
   end
 
   # correct for Aces
