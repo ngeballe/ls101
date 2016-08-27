76a843a290f88ace9bf350166c08b3391b9f4f76
Nick Geballe <ngeballe@gmail.com>
2016-08-27 09:54:11 -0400

    Take out mortgage_calculator comments

--- lesson_2_small_programs/mortgage calculator/mortgage_calculator.rb
+++ lesson_2_small_programs/mortgage calculator/mortgage_calculator.rb
@@ -1,5 +1,3 @@
-# mortgage_calculator.rb
-
 require 'pry'
 require 'yaml'
 MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
@@ -48,20 +46,16 @@ system 'clear'
 prompt(MESSAGES['welcome'])
 
 loop do
-  # get loan amount
   prompt(MESSAGES['loan_amount'])
   loan_amount = validate_input_number.to_f
 
-  # get APR
   prompt(MESSAGES['apr'])
   monthly_interest_rate = validate_input_number.to_f / 100 / 12
 
-  # get loan duration
   prompt(MESSAGES['loan_duration'])
   loan_duration_years = validate_input_number.to_f
   loan_duration_months = loan_duration_years * 12
 
-  # use formula
   payment = monthly_payment(loan_amount, monthly_interest_rate,
                             loan_duration_months)
   puts "The monthly payment is $#{format('%02.2f', payment)}."
